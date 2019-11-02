5a109922496ead45dc29d90034aefb9fab81f71a
Farai Matangira <fmatangira@gmail.com>
2019-11-02 16:11:42 -0500

    Completed quiz functionality up to the name input for highscore.

--- Homework4-Quiz/script.js
+++ Homework4-Quiz/script.js
@@ -1,6 +1,9 @@
 var interval;
-var seconds = 65;
+var seconds = 75;
 var minutesFormat;
+var score = 0;
+var i = 0;
+var optionClick;
 
 var questions = [{
     title: "Commonly used data types DO NOT include:",
@@ -9,14 +12,32 @@ var questions = [{
   },
   {
     title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses"
+    choices: ["quotes", "curly brackets", "parentheses", "alerts"],
+    answer: "curly brackets"
   },
-  ///etc.
+  {
+      title: "Which of the following is a valid type of function javascript supports?",
+      choices: ["named function", "anonymous function", "Both of the above", "None of the above"],
+      answer: "Both of the above"
+  },
+  {
+      title: "Which of the following type of variable is visible only within a function where it is defined?",
+      choices: ["global variable", "local variable", "Both of the above", "None of the above"],
+      answer: "local variable"
+  },
+  {
+      title: "Which built-in method returns the characters in a string beginning at the specified location?",
+      choices: ["substr()", "getSubstring()", "slice()", "None of the above"],
+      answer: "substr()"
+  },
+  {
+      title: "Which of the following function of String object is used to match a regular expression against a string?",
+      choices: ["concat()", "match()", "search()", "replace()"],
+      answer: "match()"
+  }
 ];
 
 $(document).ready(function() {
-
 
   //FUNCTION TO FORMAT MINUTES IN TIMER
   function formatMinutes() {
@@ -49,47 +70,93 @@ $(document).ready(function() {
   //START TIMER WHEN CLICKING ON "START QUIZ" BUTTON
   function quizStart() {
     $('#quizButton').on('click', function() {
-      $('.quizRow').show();
       $('.landingRow').hide();
-      $('#questionTitle').text(questions[1].title);
-
-        interval = setInterval(function() {
-        seconds--;
-        $('#timer').html("TIMER: " + formatMinutes() + ":" + formatSeconds());
-
-        if (seconds === 0) {
-          clearInterval(interval);
-          $('#timer').text('TIMES UP!!!');
-        }
-
-        //FORMAT COLOR OF TIMER DEPENDING ON TIME LEFT
-        $('#timer').css('color', 'green');
-
-        if (seconds < 60) {
-          $('#timer').css('color', '#e5d429');
-        }
-
-        if (seconds < 30) {
-          $('#timer').css('color', 'orange');
-        }
-
-        if (seconds < 10) {
-          $('#timer').css('color', 'red');
-        }
-      }, 1000);
+      $('.quizRow').show();
+      startTimer();
     });
   }
 
-  // function quiz() {
-  //   $('quizButton').on('click', function() {
-  //
-  //   });
-  //
-  // }
+  function quizQuestions() {
 
+    optionClick = '#option' + questions[i].answer;
+
+    $('#questionTitle').text(questions[i].title);
+    $('.option1').text(questions[i].choices[0]);
+    $('.option2').text(questions[i].choices[1]);
+    $('.option3').text(questions[i].choices[2]);
+    $('.option4').text(questions[i].choices[3]);
+    }
+
+    $('.option').click(function(event) {
+      event.preventDefault();
+
+      if (i<5) {
+        if (event.currentTarget.textContent === questions[i].answer) {
+          alert('CORRECT ANSWER!');
+          i++;
+          score++;
+          quizQuestions();
+        } else {
+          alert('WRONG ANSWER');
+          i++;
+          quizQuestions();
+          seconds = seconds-10;
+        }
+      } else {
+        if (event.currentTarget.textContent === questions[i].answer) {
+          alert('CORRECT ANSWER!');
+          score++;
+          clearInterval(interval);
+          $('.quizRow').hide();
+          showScore();
+          $('.enterNameCont').show();
+        } else {
+          alert('WRONG ANSWER');
+          seconds = seconds-10;
+          clearInterval(interval);
+          $('.quizRow').hide();
+          showScore();
+          $('.enterNameCont').show();
+        }
+      }
+  });
+
+  function startTimer() {
+    interval = setInterval(function() {
+      seconds--;
+      $('#timer').html("TIMER: " + formatMinutes() + ":" + formatSeconds());
+
+      if (seconds === 0) {
+        clearInterval(interval);
+        $('#timer').text('TIMES UP!!!');
+      }
+
+      //FORMAT COLOR OF TIMER DEPENDING ON TIME LEFT
+      $('#timer').css('color', 'green');
+
+      if (seconds < 60) {
+        $('#timer').css('color', '#e5d429');
+      }
+
+      if (seconds < 30) {
+        $('#timer').css('color', 'orange');
+      }
+
+      if (seconds < 10) {
+        $('#timer').css('color', 'red');
+      }
+    }, 1000);
+  }
+
+  function showScore() {
+    $('#score').text('You received a score of: ' + Math.round(((score/6)*100)) + '%!');
+    $('#timeLeft').text('You finished this quiz in ' + (65 - seconds) + ' seconds!');
+  }
+
+  //RUN QUIZ
   $('.quizRow').hide();
-  // $('landingRow').hide();
-  quizStart();
-  // quiz();
 
+  quizStart();
+  quizQuestions();
+  $('.enterNameCont').hide();
 });
